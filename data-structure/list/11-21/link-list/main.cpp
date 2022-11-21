6b6b33221ad9fc71f595fc3baafc3504f913e26f
jminds <jh13290900214@163.com>
2022-11-22 00:00:41 +0800

    2022.11.22

---
+++ data-structure/list/11-21/link-list/main.cpp
@@ -0,0 +1,224 @@
+#include <stdio.h>
+#include <malloc.h>
+
+typedef int ElemType;
+
+typedef struct LinkNode {
+	ElemType data;
+	struct LinkNode* next;
+}LinkNode;
+
+void createLinkListF(LinkNode*& L, ElemType a[], int n) {
+	L = (LinkNode*)malloc(sizeof(LinkNode));
+
+	LinkNode* s;
+
+	L->next = NULL;
+
+	for (int i = 0; i < n; i++)
+	{
+		s = (LinkNode*)malloc(sizeof(LinkNode));
+		s->data = a[i];
+		s->next = L->next;
+		L->next = s;
+	}
+}
+
+void createLinkListR(LinkNode*& L, ElemType a[], int n) {
+	L = (LinkNode*)malloc(sizeof(LinkNode));
+
+	LinkNode* s, * r;
+
+	r = L;
+
+	for (int i = 0; i < n; i++)
+	{
+		s = (LinkNode*)malloc(sizeof(LinkNode));
+		s->data = a[i];
+		r->next = s;
+		r = s;
+	}
+
+	r->next = NULL;
+}
+
+void initLinkList(LinkNode*& L) {
+	L = (LinkNode*)malloc(sizeof(LinkNode));
+
+	L->next = NULL;
+}
+
+void destoryLinkList(LinkNode*& L) {
+	LinkNode* pre = L, * p = L->next;
+
+	while (p != NULL)
+	{
+		free(pre);
+		pre = p;
+		p = p->next;
+	}
+
+	free(pre);
+}
+
+bool isEmptyLinkList(LinkNode* L) {
+	return L->next == NULL;
+}
+
+int lengthLinkList(LinkNode* L) {
+	LinkNode* p = L->next;
+
+	int i = 0;
+
+	while (p != NULL)
+	{
+		i++;
+		p = p->next;
+	}
+
+	return i;
+}
+
+void dispLinkList(LinkNode* L) {
+	LinkNode* p = L->next;
+
+	while (p != NULL)
+	{
+		printf("%d\n", p->data);
+		p = p->next;
+	}
+}
+
+bool getElemLinkList(LinkNode* L, int i, ElemType& e) {
+	if (i < 1)
+	{
+		return false;
+	}
+
+	LinkNode* p = L;
+
+	int j = 0;
+
+	while (p != NULL && j < i)
+	{
+		p = p->next;
+		j++;
+	}
+
+	if (p == NULL)
+	{
+		return false;
+	}
+
+	e = p->data;
+
+	return true;
+}
+
+int getLocateLinkList(LinkNode* L, ElemType e) {
+	LinkNode* p = L->next;
+
+	int j = 1;
+
+	while (p != NULL)
+	{
+		if (e == p->data) {
+			return j;
+		}
+		j++;
+		p = p->next;
+	}
+
+	return 0;
+}
+
+bool insertLinkList(LinkNode*& L, int i, ElemType e) {
+	if (i < 1)
+	{
+		return false;
+	}
+
+	LinkNode* p = L, * s;
+
+	int j = 0;
+
+	while (p != NULL && j < i - 1)
+	{
+		p = p->next;
+		j++;
+	}
+
+	if (p == NULL)
+	{
+		return false;
+	}
+
+	s = (LinkNode*)malloc(sizeof(LinkNode));
+
+	s->data = e;
+
+	s->next = p->next;
+
+	p->next = s;
+
+	return true;
+}
+
+bool deleteLinkList(LinkNode*& L, int i, ElemType& e) {
+	if (i < 1)
+	{
+		return false;
+	}
+
+	LinkNode* p = L, * q;
+
+	int j = 0;
+
+	while (p != NULL && j < i - 1)
+	{
+		p = p->next;
+		j++;
+	}
+
+	if (p == NULL)
+	{
+		return false;
+	}
+
+	q = L->next;
+
+	if (q == NULL)
+	{
+		return false;
+	}
+
+	e = q->data;
+
+	p->next = q->next;
+
+	free(q);
+
+	return true;
+}
+
+int main() {
+	ElemType a[] = { 1,4,7 };
+
+	LinkNode* L;
+
+	createLinkListR(L, a, 3);
+
+	ElemType e;
+
+	getElemLinkList(L, 2, e);
+
+	getLocateLinkList(L, 2);
+
+	insertLinkList(L, 4, 2);
+
+	deleteLinkList(L, 1, e);
+
+	dispLinkList(L);
+
+	return 0;
+}
