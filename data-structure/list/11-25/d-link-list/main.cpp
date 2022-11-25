8a7291320331ce0c4849014b317bd783bb01cf13
jminds <jh13290900214@163.com>
2022-11-26 00:25:29 +0800

    2022.11.26

---
+++ data-structure/list/11-25/d-link-list/main.cpp
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <malloc.h>
+
+typedef int ElemType;
+
+typedef struct DLinkNode {
+	ElemType data;
+	struct DLinkNode* prev;
+	struct DLinkNode* next;
+}DLinkNode;
+
+void createDLinkListF(DLinkNode*& L, ElemType a[], int n) {
+	L = (DLinkNode*)malloc(sizeof(DLinkNode));
+
+	DLinkNode* s;
+
+	L->prev = L->next = NULL;
+
+	for (int i = 0; i < n; i++)
+	{
+		s = (DLinkNode*)malloc(sizeof(DLinkNode));
+		s->data = a[i];
+		s->prev = L;
+		s->next = L->next;
+		if (L->next != NULL)
+		{
+			L->next->prev = s;
+		}
+		L->next = s;
+	}
+}
+
+void createDLinkListR(DLinkNode*& L, ElemType a[], int n) {
+	L = (DLinkNode*)malloc(sizeof(DLinkNode));
+
+	DLinkNode* s, * r;
+
+	r = L;
+
+	r->prev = NULL;
+
+	for (int i = 0; i < n; i++)
+	{
+		s = (DLinkNode*)malloc(sizeof(DLinkNode));
+		s->data = a[i];
+		s->prev = r;
+		r->next = s;
+		r = s;
+	}
+
+	r->next = NULL;
+}
+
+bool insertDLinkList(DLinkNode*& L, int i, ElemType e) {
+	if (i < 1)
+	{
+		return false;
+	}
+
+	DLinkNode* p = L, * s;
+
+	int j = 0;
+
+	while (p != NULL && j < i - 1)
+	{
+		p = p->next;
+		j++;
+	}
+
+	if (p == NULL)
+	{
+		return false;
+	}
+
+	s = (DLinkNode*)malloc(sizeof(DLinkNode));
+
+	s->data = e;
+
+	s->prev = p;
+
+	s->next = p->next;
+
+	if (p->next != NULL)
+	{
+		p->next->prev = s;
+	}
+
+	p->next = s;
+
+	return true;
+}
+
+bool deleteElemDLinkList(DLinkNode*& L, int i, ElemType& e) {
+	if (i < 1)
+	{
+		return false;
+	}
+
+	DLinkNode* p = L, * q;
+
+	int j = 0;
+
+	while (p != NULL && j < i - 1)
+	{
+		p = p->next;
+		j++;
+	}
+
+	if (p == NULL)
+	{
+		return false;
+	}
+
+	q = p->next;
+
+	if (q == NULL)
+	{
+		return false;
+	}
+
+	e = q->data;
+
+	p->next = q->next;
+
+	if (q->next != NULL)
+	{
+		q->next->prev = p;
+	}
+
+	free(q);
+
+	return true;
+}
+
+int main() {
+	return 0;
+}
---
+++ data-structure/list/11-25/sq-list/main.cpp
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <malloc.h>
+
+#define MaxSize 50
+
+typedef int ElemType;
+
+typedef struct SqList {
+	ElemType data[MaxSize];
+	int length;
+}SqList;
+
+void createSqList(SqList*& L, ElemType a[], int n) {
+	L = (SqList*)malloc(sizeof(SqList));
+
+	int k = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		L->data[k] = a[i];
+		k++;
+	}
+
+	L->length = k;
+}
+
+void initSqList(SqList*& L) {
+	L = (SqList*)malloc(sizeof(SqList));
+
+	L->length = 0;
+}
+
+void destorySqList(SqList*& L) {
+	free(L);
+}
+
+bool isEmptySqList(SqList* L) {
+	return L->length == 0;
+}
+
+int lengthSqList(SqList* L) {
+	return L->length;
+
+}
+
+void dispSqList(SqList* L) {
+	for (int i = 0; i < L->length; i++)
+	{
+		printf("%d", L->data[i]);
+	}
+}
+
+bool getElemSqList(SqList* L, int i, ElemType& e) {
+	if (i < 1 || i > L->length)
+	{
+		return false;
+	}
+
+	e = L->data[i - 1];
+
+	return true;
+}
+
+int getLocateSqList(SqList* L, ElemType e) {
+	for (int i = 0; i < L->length; i++)
+	{
+		if (e == L->data[i])
+		{
+			return i + 1;
+		}
+	}
+
+	return 0;
+}
+
+bool insertSqList(SqList*& L, int i, ElemType e) {
+	if (i < 1 || i > L->length + 1 || L->length == MaxSize)
+	{
+		return false;
+	}
+
+	for (int j = L->length - 1; j > i - 1; i--)
+	{
+		L->data[j + 1] = L->data[j];
+	}
+
+	L->data[i - 1] = e;
+
+	L->length++;
+
+	return true;
+}
+
+bool deleteSqList(SqList*& L, int i, ElemType& e) {
+	if (i < 1 || i > L->length)
+	{
+		return false;
+	}
+
+	e = L->data[i - 1];
+
+	for (int j = i; j < L->length; j++)
+	{
+		L->data[j - 1] = L->data[j];
+	}
+
+	L->length--;
+
+	return true;
+}
+
+int main() {
+	return 0;
+}
